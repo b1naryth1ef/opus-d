28cf7f6b77f536ddffa9f5efffe2c65453bc0497
Andrei <b1naryth1ef@gmail.com>
2016-08-13 04:43:28 -0500

    Fix name, more settings

--- src/opus/encoder.d
+++ src/opus/encoder.d
@@ -8,7 +8,17 @@ enum Application {
   LOWDELAY = 2051,
 }
 
+enum Bandwidth {
+  NARROWBAND = 1101,
+  MEDIUMBAND = 1102,
+  WIDEBAND = 1103,
+  SUPERWIDEBAND = 1104,
+  FULLBAND = 1105,
+}
+
 enum CTL {
+  SET_BITRATE_REQUEST = 4002,
+  SET_BANDWIDTH_REQUEST = 4008,
   SET_INBAND_FEC_REQUEST = 4012,
   SET_PACKET_LOSS_PERC_REQUEST = 4014,
 }
@@ -18,10 +28,7 @@ extern (C) {
 
   OpusEncoder* opus_encoder_create(int, int, int, int*);
   void opus_encoder_destroy(OpusEncoder*);
-
   int opus_encode(OpusEncoder*, const short*, int, ubyte*, int);
-
-  // CTLs
   int opus_encoder_ctl(OpusEncoder*, int request, ...);
 }
 
@@ -47,16 +54,24 @@ class Encoder {
   ubyte[] encode(const short[] pcm, int frameSize) {
     ubyte[] data;
     data.length = (frameSize * this.channels) * 2;
-    opus_encode(this.encoder, &pcm[0], frameSize, &data[0], cast(int)data.length);
-    return data;
+    int size = opus_encode(this.encoder, &pcm[0], frameSize, &data[0], cast(int)data.length);
+    return data[0..size];
+  }
+
+  void setBitrate(int kbps) {
+    assert(opus_encoder_ctl(this.encoder, CTL.SET_BITRATE_REQUEST, kbps * 1024) == OPUS_OK);
+  }
+
+  void setBandwidth(Bandwidth bw) {
+    assert(opus_encoder_ctl(this.encoder, CTL.SET_BANDWIDTH_REQUEST, cast(int)bw) == OPUS_OK);
   }
 
   void setInbandFEC(bool enabled) {
-    opus_encoder_ctl(this.encoder, CTL.SET_INBAND_FEC_REQUEST, cast(int)enabled);
+    assert(opus_encoder_ctl(this.encoder, CTL.SET_INBAND_FEC_REQUEST, cast(int)enabled) == OPUS_OK);
   }
 
   void setPacketLossPercent(int percent) {
-    opus_encoder_ctl(this.encoder, CTL.SET_PACKET_LOSS_PERC_REQUEST, percent);
+    assert(opus_encoder_ctl(this.encoder, CTL.SET_PACKET_LOSS_PERC_REQUEST, percent) == OPUS_OK);
   }
 
   ~this() {
@@ -68,5 +83,7 @@ unittest {
   Encoder enc = new Encoder();
   enc.setInbandFEC(true);
   enc.setPacketLossPercent(30);
+  enc.setBandwidth(Bandwidth.FULLBAND);
+  enc.setBitrate(128);
   enc.destroy();
 }
